098ecd6839a104b61a2cbaea745ad7096f17368a
Beatrizi <beatrici25@gmail.com>
2022-06-09 20:26:49 +0100

    funcion evento que toma el valor del input con .target.value

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const parrafo = document.querySelector('p');
+const parrafo = document.querySelector('p');//queremos ver el cambio
 
 const input = document.querySelector('input');
+
+//anadiendo eventlistener to const input
+/** change  (cada vez que cambie algo en el input se activa*/
+/**tomar el valor del input con .target.value */
+input.addEventListener('change', evento)
+
+function evento(eventoInput) {
+    parrafo.textContent = eventoInput.target.value;
+}
+
 
